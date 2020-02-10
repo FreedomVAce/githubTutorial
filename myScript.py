ab9a2125585603ad7810ba7af8de2e3335892926
FreedomVAce <austinyu504@gmail.com>
2020-02-10 11:28:50 -0800

    fixed again

--- myScript.py
+++ myScript.py
@@ -1,10 +1,12 @@
 import numpy as np 
 
-def my_fun(mat1,mat2)
-
-    return mat1 * mat2
+def my_fun(mat1,mat2):
+    try:
+        return mat1 * mat2
+    except ValueError:
+        return None
 
 my_martix = np.identity(4)
-my_2_martix = np.identiy(4)
+my_2_martix = np.identity(4)
 
-print(myfun(my_martix,my_2_martix))
+print(my_fun(my_martix,my_2_martix))
