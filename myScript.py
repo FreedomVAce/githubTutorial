2d3892ac6f7f38e1c38e65763cbdc0e759b7fdbf
FreedomVAce <austinyu504@gmail.com>
2020-02-10 11:54:18 -0800

    reversed

--- myScript.py
+++ myScript.py
@@ -1,16 +1,7 @@
 import numpy as np 
 import pandas as pd
 
-def my_fun(mat1,mat2):
-    try:
-        return mat1 * mat2
-    except ValueError:
-        print("Matrixs not here")
-        return None
-
-
 
 my_martix = np.identity(5)
-my_2_martix = np.random.random(size = 4,4)
-print(my_fun(my_martix,my_2_martix))
-
+my_2_martix = np.random.random(size = 5,4)
+print( my_martix)
