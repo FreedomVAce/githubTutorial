4d38eab1a8d57b1d9d20ccdf7ef76db22c94f8f8
FreedomVAce <austinyu504@gmail.com>
2020-02-10 11:26:39 -0800

    fixed the function

--- myScript.py
+++ myScript.py
@@ -1,4 +1,10 @@
 import numpy as np 
 
+def my_fun(mat1,mat2)
+
+    return mat1 * mat2
+
 my_martix = np.identity(4)
-print( my_martix)
+my_2_martix = np.identiy(4)
+
+print(myfun(my_martix,my_2_martix))
